d7c7c169c860ee03eecc91ea4e89b9880a0fb9b8
UrFriendKen <51278666+UrFriendKen@users.noreply.github.com>
2023-08-17 16:42:46 +0800

    Init

---
+++ Main.cs
@@ -0,0 +1,35 @@
+﻿using KitchenMods;
+using UnityEngine;
+
+// Namespace should have "Kitchen" in the beginning
+namespace KitchenSmartGrabberPullBugFix
+{
+    public class Main : IModInitializer
+    {
+        public const string MOD_GUID = $"IcedMilo.PlateUp.{MOD_NAME}";
+        public const string MOD_NAME = "Smart Grabber Pull Bug Fix";
+        public const string MOD_VERSION = "0.1.0";
+
+        public void PostActivate(KitchenMods.Mod mod)
+        {
+            LogWarning($"{MOD_GUID} v{MOD_VERSION} in use!");
+        }
+
+        public void PreInject()
+        {
+        }
+
+        public void PostInject()
+        {
+        }
+
+        #region Logging
+        public static void LogInfo(string _log) { Debug.Log($"[{MOD_NAME}] " + _log); }
+        public static void LogWarning(string _log) { Debug.LogWarning($"[{MOD_NAME}] " + _log); }
+        public static void LogError(string _log) { Debug.LogError($"[{MOD_NAME}] " + _log); }
+        public static void LogInfo(object _log) { LogInfo(_log.ToString()); }
+        public static void LogWarning(object _log) { LogWarning(_log.ToString()); }
+        public static void LogError(object _log) { LogError(_log.ToString()); }
+        #endregion
+    }
+}
---
+++ ReplaceMisgrabbedItem.cs
@@ -0,0 +1,85 @@
+﻿using Kitchen;
+using KitchenData;
+using KitchenMods;
+using Unity.Collections;
+using Unity.Entities;
+
+namespace KitchenSmartGrabberPullBugFix
+{
+    public class ReplaceMisgrabbedItem : DaySystem, IModSystem
+    {
+        EntityQuery Grabbers;
+
+        protected override void Initialise()
+        {
+            base.Initialise();
+            Grabbers = GetEntityQuery(typeof(CConveyPushItems), typeof(CItemHolder));
+        }
+
+        protected override void OnUpdate()
+        {
+            using NativeArray<Entity> entities = Grabbers.ToEntityArray(Allocator.Temp);
+            using NativeArray<CConveyPushItems> pushItems = Grabbers.ToComponentDataArray<CConveyPushItems>(Allocator.Temp);
+            using NativeArray<CItemHolder> holders = Grabbers.ToComponentDataArray<CItemHolder>(Allocator.Temp);
+
+            EntityContext ctx = new EntityContext(EntityManager);
+
+            for (int i = 0; i < entities.Length; i++)
+            {
+                Entity entity = entities[i];
+                CConveyPushItems push = pushItems[i];
+                CItemHolder holder = holders[i];
+
+                if (push.State != CConveyPushItems.ConveyState.Grab || !push.GrabSpecificType || push.SpecificType == 0 || holder.HeldItem == default || !Require(holder.HeldItem, out CItem item))
+                    continue;
+
+                bool requireFix = item.ID != push.SpecificType || item.Items.Count != push.SpecificComponents.Count;
+
+                if (!requireFix)
+                {
+                    for (int j = 0; j < item.Items.Count; j++)
+                    {
+                        int componentID = item.Items[j];
+                        bool foundComponent = false;
+                        for (int k = 0; k < push.SpecificComponents.Count; k++)
+                        {
+                            if (componentID == push.SpecificComponents[k])
+                            {
+                                foundComponent = true;
+                                break;
+                            }
+                        }
+                        if (!foundComponent)
+                        {
+                            requireFix = true;
+                            break;
+                        }
+                    }
+                }
+                if (!requireFix)
+                    continue;
+
+                string origItemName = "Unknown";
+                if (GameData.Main.TryGet(item.ID, out Item origItem))
+                    origItemName = origItem.name;
+                Main.LogError($"Misgrabbed item detected! ({origItemName})");
+                if (!GameData.Main.TryGet(push.SpecificType, out Item itemGDO, warn_if_fail: true))
+                    continue;
+                EntityManager.DestroyEntity(holder.HeldItem);
+
+                bool isGroup = itemGDO is ItemGroup;
+                Entity newItem;
+                if (isGroup)
+                {
+                    newItem = ctx.CreateItemGroup(push.SpecificType, push.SpecificComponents);
+                }
+                else
+                {
+                    newItem = ctx.CreateItem(push.SpecificType);
+                }
+                ctx.UpdateHolder(newItem, entity);
+                Main.LogWarning($"Replaced misgrabbed item with {itemGDO.name}.");
+            }
+        }
+    }
+}
